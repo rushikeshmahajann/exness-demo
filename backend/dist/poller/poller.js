ff0c242e9cb081f416676044ee3c272760fc962d
rushikeshmahajann <iamrushi05@gmail.com>
2025-08-31 16:10:48 +0530

    feat: price poller with redis queue

--- backend/dist/poller/poller.js
+++ backend/dist/poller/poller.js
@@ -1,14 +1,28 @@
 import WebSocket from "ws";
-const symbol = "btcusdt";
-const wsUrl = `wss://stream.binance.com:9443/ws/${symbol}@trade`;
+import { createClient } from 'redis';
+const wsUrl = `wss://stream.binance.com:9443/stream?streams=btcusdt@trade/ethusdt@trade/solusdt@trade`;
+const redis = createClient({
+    url: "redis://localhost:6379",
+});
+redis.on("error", (err) => console.error("Redis Client Error", err));
+await redis.connect();
 const ws = new WebSocket(wsUrl);
 ws.on("open", () => {
-    console.log(`Connected to Binance ${symbol} trades`);
+    console.log(`Connected to Binance trades`);
 });
-ws.on("message", (msg) => {
-    const data = JSON.parse(msg.toString());
-    console.log("📈 Trade:", data);
+ws.on("message", async (msg) => {
+    try {
+        const payload = JSON.parse(msg.toString());
+        const trade = payload.data;
+        await redis.lPush("trades_queue", JSON.stringify(trade));
+    }
+    catch (error) {
+        console.error("Failed to push trade to Redis:", error);
+    }
 });
 ws.on("error", (err) => {
-    console.error("Error:", err);
+    console.error("Websocket Error:", err);
+});
+ws.on("close", () => {
+    console.log("websocket disconnected, consider reconnect logic");
 });
